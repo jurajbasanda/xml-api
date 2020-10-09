59d4635e5c2901aa3e1a7decbf8e8ff3b70e45f3
Juraj Basanda <basanda.juraj@gmail.com>
2020-10-09 19:50:10 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
 
-function App() {
+const App= () => {
+  const [Data,setData] = useState({})
+  const fetch = async () =>{ 
+    try{
+    const res = await axios.get('./data.xml') 
+             await setData(res.data)
+    let parser = new DOMParser()
+    let xmlDOM = parser.parseFromString(Data,'application/xml')
+          console.log(xmlDOM)
+          console.log(Data)
+        }
+    catch(error){
+      console.log(error.message)
+    }
+      }
+  useEffect(() => {
+   fetch()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <pre>
+    </pre>
+ </div>
   );
 }
 
